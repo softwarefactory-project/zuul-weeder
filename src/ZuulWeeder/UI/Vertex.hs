f53059dcb61c894627e178905d85629310fe1caa
Tristan Cacqueray <tdecacqu@redhat.com>
2024-03-18 13:38:31 +0000

    Handle gitlab url used in git connection for project location
    This change fixes the gitlab links for project when the connection is git.
    
    Change-Id: Ife4595941bdf6f1e79d75079f16e717ac8ac9250

--- src/ZuulWeeder/UI/Vertex.hs
+++ src/ZuulWeeder/UI/Vertex.hs
@@ -89,7 +89,7 @@ projectUrl curl proj = case curl of
   GitlabUrl url -> buildGitlabUrl url
   PagureUrl url -> buildPagureUrl url
   GitUrl url
-    | "gitlab.com" `Text.isInfixOf` url -> buildGitlabUrl url
+    | "gitlab" `Text.isInfixOf` url -> buildGitlabUrl url
     | "github.com" `Text.isInfixOf` url -> buildGithubUrl url
     | "pagure.io" `Text.isInfixOf` url -> buildPagureUrl url
     | "src.fedoraproject.io" `Text.isInfixOf` url -> buildPagureUrl url
